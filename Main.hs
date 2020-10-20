0f7ece1a12ba74a4f9c33238b7c1dfcb8dc8ceb0
Ulf Norell <ulf.norell@gmail.com>
2020-10-20 16:17:24 +0200

    First stab at generating types

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import Control.Monad.Fail (MonadFail)
 import Control.Monad.IO.Class
 import Data.List
 import Data.Maybe
+import Data.Map (Map)
 import qualified Data.Map as Map
 import System.Console.GetOpt
 import System.Environment
@@ -15,6 +16,7 @@ import Agda.Compiler.Common
 import Agda.Interaction.BasicOps
 import Agda.TypeChecking.Monad
 import Agda.TypeChecking.Pretty
+import Agda.Syntax.Common
 import Agda.Syntax.Internal hiding (sort, set)
 import Agda.Syntax.Position
 import Agda.Syntax.Translation.ConcreteToAbstract
@@ -65,8 +67,64 @@ compile _ _ _ def = getUniqueCompilerPragma pragmaName (defName def) >>= \ case
   Just _  -> compile' def
 
 compile' :: Definition -> TCM CompiledDef
-compile' def = return [(nameBindingSite x, prettyShow x ++ " = undefined\n")]
+compile' def =
+  case theDef def of
+    Function{} -> compileFun r hsName def
+    _          -> return []
   where x = qnameName $ defName def
+        r = nameBindingSite x
+        hsName = prettyShow x
+
+compileClause :: String -> Clause -> TCM String
+compileClause x c = return $ x ++ " = undefined"
+
+paren True  s = "(" ++ s ++ ")"
+paren False s = s
+
+data PrimType = Nat | List | Unit
+  deriving (Show, Eq)
+
+type Builtins = Map QName PrimType
+
+getBuiltins :: TCM Builtins
+getBuiltins = Map.fromList . concat <$> mapM getB [(builtinNat, Nat), (builtinList, List), (builtinUnit, Unit)]
+  where
+    getB (b, t) = getBuiltin' b >>= \ case
+      Nothing        -> return []
+      Just (Def q _) -> return [(q, t)]
+
+renderPrimType :: PrimType -> [String] -> String
+renderPrimType Nat  []  = "Integer"
+renderPrimType List [a] = "[" ++ a ++ "]"
+renderPrimType Unit []  = "()"
+
+compileType :: Builtins -> Int -> Term -> TCM String
+compileType builtins p t = do
+  t <- reduce t
+  case t of
+    Pi a b | hidden a -> underAbstraction a b (compileType builtins p . unEl)
+             -- Hidden Pi means Haskell forall
+    Pi a (NoAbs _ b) | visible a -> do
+      hsA <- compileType builtins 1 (unEl $ unDom a)
+      hsB <- compileType builtins 0 (unEl b)
+      return $ paren (p > 0) (hsA ++ " -> " ++ hsB)
+    Def f es | Just prim <- Map.lookup f builtins
+             , Just args <- allApplyElims es -> do
+      vs <- mapM (compileType builtins 0 . unArg) $ filter visible args
+      return $ renderPrimType prim vs
+    t@(Var _ []) -> show <$> prettyTCM t
+    t -> genericDocError =<< text "Bad Haskell type:" <?> prettyTCM t
+
+compileFun :: Range -> String -> Definition -> TCM CompiledDef
+compileFun r x def = do
+  builtins <- getBuiltins
+  ty <- compileType builtins 0 (unEl $ defType def)
+  cs <- mapM (compileClause x) funClauses
+  let code = x ++ " :: " ++ ty ++ "\n" ++
+             unlines cs
+  return [(r, code)]
+  where
+    Function{..} = theDef def
 
 renderCode :: [(Range, String)] -> String
 renderCode = unlines . map snd . sort
--- Test.agda
+++ Test.agda
@@ -13,6 +13,12 @@ variable
   import Prelude
  #-}
 
+data Exp : Set where
+  plus : Exp → Exp → Exp
+  int : Nat → Exp
+
+{-# COMPILE AGDA2HS Exp #-}
+
 sum : List Nat → Nat
 sum []       = 0
 sum (x ∷ xs) = x + sum xs
@@ -21,16 +27,17 @@ sum (x ∷ xs) = x + sum xs
 
 {-# FOREIGN AGDA2HS -- comment #-}
 
-_++_ : List a → List a → List a
-[]       ++ ys = ys
-(x ∷ xs) ++ ys = x ∷ (xs ++ ys)
+append : List a → List a → List a
+append []       ys = ys
+append (x ∷ xs) ys = x ∷ append xs ys
 
-{-# COMPILE AGDA2HS _++_ #-}
+{-# COMPILE AGDA2HS append #-}
 
 assoc : (a b c : Nat) → a + (b + c) ≡ (a + b) + c
 assoc zero    b c = refl
 assoc (suc a) b c rewrite assoc a b c = refl
 
-thm : ∀ xs ys → sum (xs ++ ys) ≡ sum xs + sum ys
+thm : ∀ xs ys → sum (append xs ys) ≡ sum xs + sum ys
 thm []       ys = refl
 thm (x ∷ xs) ys rewrite thm xs ys | assoc x (sum xs) (sum ys) = refl
+
