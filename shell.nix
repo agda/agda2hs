0f8b0b8a17ecad9e9be7ef5cae564187787df31d
James Chapman <james.chapman@iohk.io>
2020-10-21 23:16:35 +0100

    added a shell.nix generated by cabal2nix

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, Agda, base, containers, stdenv }:
+      mkDerivation {
+        pname = "agda2hs";
+        version = "0.1";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ Agda base containers ];
+        description = "Compiling Agda code to readable Haskell";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
