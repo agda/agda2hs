52c77faaa2018ea16ca20dd45d875c429b894b19
Jesper Cockx <jesper@sikanda.be>
2022-02-19 13:54:47 +0100

    Fix AllTests.hs golden value

--- test/golden/AllTests.hs
+++ test/golden/AllTests.hs
@@ -2,6 +2,7 @@ module AllTests where
 
 import Issue14
 import Issue65
+import Issue69
 import Issue73
 import Fixities
 import LanguageConstructs
@@ -13,8 +14,11 @@ import Tree
 import Tuples
 import Where
 import TypeSynonyms
+import ConstrainedInstance
 import Datatypes
 import Records
+import Default
 import DefaultMethods
 import Vector
+import Superclass
 
