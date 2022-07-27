63e6a0e6d673421501c92f06163120ec903b30c2
Jesper Cockx <jesper@sikanda.be>
2022-07-27 13:59:19 +0200

    Clean up imports

--- src/Agda2Hs/HsUtils.hs
+++ src/Agda2Hs/HsUtils.hs
@@ -1,9 +1,9 @@
 
 module Agda2Hs.HsUtils where
 
-import Data.Data (Data)
-import Data.Generics (listify, everywhere, mkT, extT)
-import Data.Map (Map)
+import Data.Data ( Data )
+import Data.Generics ( listify, everywhere, mkT, extT )
+import Data.Map ( Map )
 import qualified Data.Map as Map
 
 import Language.Haskell.Exts.SrcLoc
@@ -14,9 +14,10 @@ import Language.Haskell.Exts.Pretty
 import Language.Haskell.Exts.Fixity
 
 import Agda.Syntax.Position
+
 import Agda.Utils.FileName
 import Agda.Utils.List
-import Agda.Utils.Maybe.Strict (toStrict)
+import Agda.Utils.Maybe.Strict ( toStrict )
 
 -- Names ------------------------------------------------------------------
 
