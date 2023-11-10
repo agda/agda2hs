dc35198740f62196c8f3c730cf5d1dad2d2d046d
flupe <lucas@escot.me>
2023-11-10 21:58:52 +0100

    set range for all compiled definitions

--- src/Agda2Hs/Compile.hs
+++ src/Agda2Hs/Compile.hs
@@ -21,7 +21,7 @@ import Agda2Hs.Compile.Postulate ( compilePostulate )
 import Agda2Hs.Compile.Record ( compileRecord, checkUnboxPragma )
 import Agda2Hs.Compile.Rewrites
 import Agda2Hs.Compile.Types
-import Agda2Hs.Compile.Utils ( tellExtension )
+import Agda2Hs.Compile.Utils ( setCurrentRangeQ, tellExtension )
 import Agda2Hs.Pragma
 
 -- Needs a list of rewrite rules too.
@@ -55,45 +55,46 @@ compile opts tlm _ def = withCurrentModule (qnameModule $ defName def) $ runC tl
     single x = [x]
 
     compileAndTag :: C CompiledDef
-    compileAndTag = processPragma (defName def) >>= \ p -> do
-      reportSDoc "agda2hs.compile" 5 $
-        text "Compiling definition: " <+> prettyTCM (defName def)
-      reportSDoc "agda2hs.compile" 45 $
-        text "Pragma: " <+> text (show p)
-      reportSDoc "agda2hs.compile" 45 $
-        text "Compiling definition: " <+> pretty (theDef def)
-      case (p , defInstance def , theDef def) of
-        (NoPragma, _, _) ->
-          return []
-        (ExistingClassPragma, _, _) ->
-          return [] -- No code generation, but affects how projections are compiled
-        (UnboxPragma s, _, defn) ->
-          checkUnboxPragma defn >> return [] -- also no code generation
-        (TransparentPragma  , _, Function{}) ->
-          checkTransparentPragma def >> return [] -- also no code generation
-        (ClassPragma ms, _, Record{}) ->
-          tag . single <$> compileRecord (ToClass ms) def
-        (NewTypePragma ds, _, Record{}) ->
-          tag . single <$> compileRecord (ToRecordNewType ds) def
-        (NewTypePragma ds, _, Datatype{}) ->
-          tag <$> compileData ToDataNewType ds def
-        (DefaultPragma ds, _, Datatype{}) ->
-          tag <$> compileData ToData ds def
-        (DerivePragma s, Just _, _) ->
-          tag . single <$> compileInstance (ToDerivation s) def
-        (DefaultPragma _, Just _, Axiom{}) ->
-          tag . single <$> compileInstance (ToDerivation Nothing) def
-        (DefaultPragma _, Just _, _) ->
-          tag . single <$> compileInstance ToDefinition def
-        (DefaultPragma _, _, Axiom{}) ->
-          tag <$> compilePostulate def
-        (DefaultPragma _, _, Function{}) ->
-          tag <$> compileFun True def
-        (DefaultPragma ds, _, Record{}) ->
-          tag . single <$> compileRecord (ToRecord ds) def
-        _ ->
-          genericDocError =<< do
-          text "Don't know how to compile" <+> prettyTCM (defName def)
+    compileAndTag = processPragma (defName def) >>= \ p -> 
+      setCurrentRangeQ (defName def) $ do
+        reportSDoc "agda2hs.compile" 5 $
+          text "Compiling definition: " <+> prettyTCM (defName def)
+        reportSDoc "agda2hs.compile" 45 $
+          text "Pragma: " <+> text (show p)
+        reportSDoc "agda2hs.compile" 45 $
+          text "Compiling definition: " <+> pretty (theDef def)
+        case (p , defInstance def , theDef def) of
+          (NoPragma, _, _) ->
+            return []
+          (ExistingClassPragma, _, _) ->
+            return [] -- No code generation, but affects how projections are compiled
+          (UnboxPragma s, _, defn) ->
+            checkUnboxPragma defn >> return [] -- also no code generation
+          (TransparentPragma  , _, Function{}) ->
+            checkTransparentPragma def >> return [] -- also no code generation
+          (ClassPragma ms, _, Record{}) ->
+            tag . single <$> compileRecord (ToClass ms) def
+          (NewTypePragma ds, _, Record{}) ->
+            tag . single <$> compileRecord (ToRecordNewType ds) def
+          (NewTypePragma ds, _, Datatype{}) ->
+            tag <$> compileData ToDataNewType ds def
+          (DefaultPragma ds, _, Datatype{}) ->
+            tag <$> compileData ToData ds def
+          (DerivePragma s, Just _, _) ->
+            tag . single <$> compileInstance (ToDerivation s) def
+          (DefaultPragma _, Just _, Axiom{}) ->
+            tag . single <$> compileInstance (ToDerivation Nothing) def
+          (DefaultPragma _, Just _, _) ->
+            tag . single <$> compileInstance ToDefinition def
+          (DefaultPragma _, _, Axiom{}) ->
+            tag <$> compilePostulate def
+          (DefaultPragma _, _, Function{}) ->
+            tag <$> compileFun True def
+          (DefaultPragma ds, _, Record{}) ->
+            tag . single <$> compileRecord (ToRecord ds) def
+          _ ->
+            genericDocError =<< do
+            text "Don't know how to compile" <+> prettyTCM (defName def)
 
     postCompile :: C ()
     postCompile =
