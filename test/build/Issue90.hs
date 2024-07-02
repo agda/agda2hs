2968b133c25324ec6db078d7257c497f8bdfa2d9
jespercockx <jespercockx@users.noreply.github.com>
2024-07-02 15:21:10 +0000

    deploy: f5ac455b0d6e4364f4195918fe1cf215a3e8c434

---
+++ test/build/Issue90.hs
@@ -0,0 +1,72 @@
+module Issue90 where
+
+import Numeric.Natural (Natural)
+
+good :: Natural
+good = bar
+  where
+    foo :: Natural
+    foo = 42
+    bar :: Natural
+    bar = foo
+
+bad :: Natural
+bad = bar
+  where
+    bar :: Natural
+    bar = foo
+    foo :: Natural
+    foo = 42
+
+good2 :: Natural
+good2 = bar
+  where
+    foo :: Natural
+    foo = 42 + x
+      where
+        x :: Natural
+        x = 1
+    bar :: Natural
+    bar = foo + x
+      where
+        x :: Natural
+        x = 2
+
+bad2 :: Natural
+bad2 = bar
+  where
+    bar :: Natural
+    bar = foo + x
+      where
+        x :: Natural
+        x = 2
+    foo :: Natural
+    foo = 42 + x
+      where
+        x :: Natural
+        x = 1
+
+test :: Bool -> Natural
+test True = bar
+  where
+    foo :: Natural
+    foo = 42 + ted
+      where
+        nes :: Natural
+        nes = 1
+        ted :: Natural
+        ted = nes + 1
+    bar :: Natural
+    bar = foo
+test False = bar
+  where
+    bar :: Natural
+    bar = foo
+    foo :: Natural
+    foo = 42 + ted
+      where
+        ted :: Natural
+        ted = nes + 1
+        nes :: Natural
+        nes = 1
+
