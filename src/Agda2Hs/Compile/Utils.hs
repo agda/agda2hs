f2fef22d382db918656995327908d7ab88283282
Jesper Cockx <jesper@sikanda.be>
2024-07-11 18:01:42 +0200

    [ refactor ] Factor out function `isPrimModule`

--- src/Agda2Hs/Compile/Utils.hs
+++ src/Agda2Hs/Compile/Utils.hs
@@ -7,6 +7,7 @@ import Control.Monad.Reader
 import Control.Monad.Writer ( tell )
 import Control.Monad.State ( put, modify )
 
+import Data.List ( isPrefixOf )
 import Data.Maybe ( isJust )
 import qualified Data.Map as M
 import Data.List ( isPrefixOf )
