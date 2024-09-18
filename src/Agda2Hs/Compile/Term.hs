41bd381cee642042ecb6207b11acf2afa27be8c6
Andreas Källberg <anka.213@gmail.com>
2024-09-18 18:55:19 +0200

    Remove args if calling out from an inner module, not into an outer module

--- src/Agda2Hs/Compile/Term.hs
+++ src/Agda2Hs/Compile/Term.hs
@@ -176,8 +176,8 @@ compileDef f ty args =
       -- if the function is called from the same module it's defined in,
       -- we drop the module parameters
       -- NOTE(flupe): in the future we're not always gonna be erasing module parameters
-      if mnameToList currentMod `isPrefixOf` mnameToList defMod then do
-        npars <- size <$> (lookupSection =<< currentModule)
+      if mnameToList defMod `isPrefixOf` mnameToList currentMod then do
+        npars <- size <$> lookupSection defMod
         let (pars, rest) = splitAt npars args
         ty' <- piApplyM ty pars
         pure (ty', rest)
