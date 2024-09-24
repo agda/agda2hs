2b72ccb752accf2906c252c28839bbcae30d6f62
Bohdan <17168103+liesnikov@users.noreply.github.com>
2024-09-24 17:10:04 +0200

    remove the jailbreak now that aeson bounds are relaxed

--- flake.nix
+++ flake.nix
@@ -20,14 +20,14 @@
             '';
             src = ./.;
           };
+        # options provides a way to jailbreak for packages that use agda2hs
         agda2hs-pkg = options:
           pkgs.haskellPackages.haskellSrc2nix {
             name = "agda2hs";
             src = ./.;
             extraCabal2nixOptions = options; #"--jailbreak"
         };
-        # jailbreaking here because otherwise aeson has to be overridden and that triggers recompilation of a lot of dependencies
-        agda2hs-hs = pkgs.haskellPackages.callPackage (agda2hs-pkg "--jailbreak") {};
+        agda2hs-hs = pkgs.haskellPackages.callPackage (agda2hs-pkg "") {};
         agda2hs-expr = import ./agda2hs.nix;
         agda2hs = pkgs.callPackage agda2hs-expr {
             inherit self;
