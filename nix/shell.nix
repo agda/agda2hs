fb8dad491aedacec346de64d9192c9c5403075bf
liesnikov <17168103+liesnikov@users.noreply.github.com>
2025-01-23 13:59:01 +0100

    switch to v1-build on the nix ci
    alex and happy aren't available for cabal v2-build in shellFor due to
    https://github.com/haskell/cabal/issues/8434
    https://github.com/NixOS/nixpkgs/issues/130556
    https://github.com/NixOS/nixpkgs/issues/176887
    
    for some reason still needs `cabal update`, but doesn't download
    anything from there

--- nix/shell.nix
+++ nix/shell.nix
@@ -3,11 +3,24 @@
   agda2hs-hs ? (import ./lib.nix { inherit pkgs; }).agda2hs-hs,
 }:
 pkgs.haskellPackages.shellFor {
+  # This doesn't result in a shell where you can use cabal (v2-)build,
+  # due to build-tool-depends in Agda's .cabal file, so for now only v1-build works
+  # Making cabal re-install alex and happy from Hackage can work,
+  # which will be done if the user runs `cabal update` and `cabal build`.
+  # relevant issues listed in:
+  # https://gist.github.com/ScottFreeCode/ef9f254e2dd91544bba4a068852fc81f
+  # main ones are:
+  # https://github.com/haskell/cabal/issues/8434
+  # https://github.com/NixOS/nixpkgs/issues/130556
+  # https://github.com/NixOS/nixpkgs/issues/176887
   packages = p: [ agda2hs-hs ];
-  buildInputs = with pkgs.haskellPackages; [
+  nativeBuildInputs = with pkgs.haskellPackages; [
+    # related to haskell
     cabal-install
-    cabal2nix
     haskell-language-server
+    # general goodies
     pkgs.agda
+    pkgs.nixfmt-rfc-style
+    cabal2nix
   ];
 }
