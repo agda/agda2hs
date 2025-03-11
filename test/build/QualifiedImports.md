f413774a78e5510fd9e1c0b25e0a6acfc6a183dd
omelkonian <omelkonian@users.noreply.github.com>
2025-03-11 14:05:10 +0000

    deploy: d91d2ed539774d39fca9fc893b3f57f495521711

---
+++ test/build/QualifiedImports.md
@@ -0,0 +1,32 @@
+```haskell
+module QualifiedImports where
+
+import qualified Importee (Foo(MkFoo), foo)
+import QualifiedImportee ()
+import qualified QualifiedImportee as Qually (Foo, Fooable(defaultFoo, doTheFoo), foo, (!#))
+
+-- ** simple qualification
+
+simpqualBar :: Int
+simpqualBar = Importee.foo
+
+simpfoo :: Importee.Foo
+simpfoo = Importee.MkFoo
+
+-- ** qualified imports
+
+qualBar :: Int
+qualBar = Qually.foo
+
+qualBaz :: Int
+qualBaz = (Qually.!#) 2 2
+
+qualFooable :: Qually.Foo
+qualFooable = Qually.doTheFoo
+
+qualDefaultBar :: Qually.Foo
+qualDefaultBar = Qually.defaultFoo
+
+type Foo = Importee.Foo
+
+```
---
+++ test/build/RequalifiedImports.hs
@@ -0,0 +1,30 @@
+module RequalifiedImports where
+
+import OtherImportee (OtherFoo(MkFoo))
+import QualifiedImportee ()
+import qualified QualifiedImportee as A (Foo, Fooable(defaultFoo, doTheFoo), foo, (!#))
+
+-- ** conflicting imports are all replaced with the "smallest" qualifier
+--   * the characters are ordered based on their ASCII value (i.e. capitals first)
+--   * the order of the imports in the file does not matter
+--   * the scope-checker has already replaced previous definitions in the file
+
+requalBar :: Int
+requalBar = A.foo
+
+requalBaz :: Int
+requalBaz = (A.!#) 2 2
+
+requalFooable :: A.Foo
+requalFooable = A.doTheFoo
+
+requalDefaultBar :: A.Foo
+requalDefaultBar = A.defaultFoo
+
+-- ** qualifying an open'ed module has no effect
+
+type T = Int
+
+otherFoo :: OtherFoo
+otherFoo = MkFoo
+
