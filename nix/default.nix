cdb5392560dd851afddfd0f2e1ab93bb23bd8063
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2025-05-02 12:18:09 +0200

    Add `containers-lib` nix package

--- nix/default.nix
+++ nix/default.nix
@@ -17,8 +17,17 @@ let
     '';
     src = ../lib/base;
   };
+
+  containers-lib = lib.mkDerivation {
+    pname = "containers";
+    meta = { };
+    version = "0.8";
+    buildInputs = [ base-lib ];
+    everythingFile = "./agda/containers.agda";
+    src = ../lib/containers;
+  };
 in
 {
   inherit (lib) agda2hs;
-  inherit base-lib;
+  inherit base-lib containers-lib;
 }
