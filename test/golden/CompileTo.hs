2a7f7846377d6bd367dcd046e7cb65ad2498413b
Jesper Cockx <jesper@sikanda.be>
2025-10-06 16:37:40 +0200

    [ re #155 ] Add vecToList + vector reverse examples to CompileTo.agda

--- test/golden/CompileTo.hs
+++ test/golden/CompileTo.hs
@@ -15,3 +15,10 @@ llength (x : xs) = 1 + llength xs
 test3 :: Natural
 test3 = llength test1
 
+lfoldl :: (b -> a -> b) -> b -> [a] -> b
+lfoldl f v [] = v
+lfoldl f v (x : xs) = lfoldl f (f v x) xs
+
+vreverse :: [a] -> [a]
+vreverse = lfoldl (\ xs x -> x : xs) []
+
