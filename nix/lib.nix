acb521e2b66cbccf3694c264929ae6298fb25637
liesnikov <17168103+liesnikov@users.noreply.github.com>
2025-11-29 17:51:14 +0100

    simplify lib.nix
    no need to expose hsrc since the options can be overridden on the
    resulting derivation too

--- nix/lib.nix
+++ nix/lib.nix
@@ -3,22 +3,14 @@
   ...
 }:
 let
-  hsrc =
-    options:
-    pkgs.haskellPackages.haskellSrc2nix {
-      name = "agda2hs";
-      src = ../.;
-      extraCabal2nixOptions = options; # "--jailbreak"
-    };
-  hpkg = (pkgs.haskellPackages.callPackage (hsrc "") {}).overrideAttrs (
+  hpkg = (pkgs.haskellPackages.callCabal2nix "agda2hs" ../. {}).overrideAttrs (
     finAttr: preAttr: {
-      # add ./dist/build/agda2hs to $PATH because
-      # cabal2nix doesn't pick up agda2hs from build-tool-depends of the test suite
-      # my (@liesnikov) guess is that since the intermediate binary
-      # is not a separate derivation it's hard to form a fixpoint on a derivation level
+      # add ./dist/build/agda2hs to $PATH
+      # because cabal2nix doesn't pick up agda2hs from build-tool-depends of agda2hs-test
+      # my (@liesnikov) guess is that since each cabal target is not a separate derivation
+      # it's hard to form a fixpoint on a derivation level
       preCheck = "export PATH=$(pwd)/dist/build/agda2hs:$PATH";
-    }
-  );
+    });
   expr = import ./agda2hs.nix;
   agda2hs = pkgs.lib.makeScope pkgs.newScope (
     self:
@@ -30,7 +22,6 @@ let
   );
 in
 {
-  agda2hs-pkg = hsrc;
   agda2hs-hs = hpkg;
   agda2hs-expr = expr;
   inherit (agda2hs) agda2hs withPackages mkDerivation;
