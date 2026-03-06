1ef0174871e58f324dbe4a65c3b701bec8dff85a
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2026-03-06 18:29:42 +0100

    Move test for equality constraint to Success directory

--- test/EqualityExample.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TypeOperators #-}
-module EqualityExample where
-
-coerce' :: a ~ b => a -> b
-coerce' x = x
-
-sameList :: x ~ y => x -> y -> [x]
-sameList vx vy = [vx, coerce' vy]
-
--- test/Succeed/EqualityConstraint.hs
+++ test/Succeed/EqualityConstraint.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE TypeOperators #-}
 module EqualityConstraint where
 
-c :: *
-c = error "postulate: *"
-
 myFunc :: a ~ b => c
 myFunc = error "postulate:   a ~ b => c"
 
